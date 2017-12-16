1d11fc9f9fdf4faa6099571218809f6685f36807
DUBOCQ Clément <mail.clement@gmail.com>
2017-12-16 18:50:54 +0100

    tests

---
+++ src/devmanager.js
@@ -0,0 +1,237 @@
+/*
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *     http://www.apache.org/licenses/LICENSE-2.0
+  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @fileoverview USB device manager.
+ *
+ * +-----------------+
+ * | Reader-specific |
+ * |   driver, like  |  The "who" in the open() function.
+ * |    scl3711.js   |
+ * +-----------------+  For low level driver, this is "client".
+ *         |
+ *         v
+ * +-----------------+
+ * |  dev_manager:   |
+ * | open and enum   |
+ * | low level devs  |
+ * +-----------------+
+ *     |         |
+ *     v         v
+ * +-------+ +-------+  The "which" in the open() function.
+ * |llSCL37| |llSCL37|
+ * |       | |       |  Low level USB driver.
+ * |       | |       |  each maps to a physical device instance.
+ * |       | |       |  handling Tx/Rx queues.
+ * +-------+ +-------+
+ *
+ */
+
+'use strict';
+
+
+// List of enumerated usb devices.
+function devManager() {
+  this.devs = [];         // array storing the low level device.
+  this.enumerators = [];  // array storing the pending callers of enumerate().
+}
+
+// Remove a device from devs[] list.
+devManager.prototype.dropDevice = function(dev) {
+  var tmp = this.devs;
+  this.devs = [];
+
+  var present = false;
+  for (var i = 0; i < tmp.length; ++i) {
+    if (tmp[i] !== dev) {
+      this.devs.push(tmp[i]);
+    } else {
+      present = true;
+    }
+  }
+  if (!present) return;  // Done.
+
+  if (dev.dev) {
+    chrome.usb.releaseInterface(dev.dev, 0,
+        function() { console.log(UTIL_fmt('released')); });
+    chrome.usb.closeDevice(dev.dev,
+        function() { console.log(UTIL_fmt('closed')); });
+    dev.dev = null;
+  }
+
+  console.log(this.devs.length + ' devices remaining');
+};
+
+// Close all enumerated devices.
+devManager.prototype.closeAll = function(cb) {
+
+  console.debug("devManager.closeAll() is called");
+
+  // First close and stop talking to any device we already
+  // have enumerated.
+  var d = this.devs.slice(0);
+  for (var i = 0; i < d.length; ++i) {
+    d[i].close();
+  }
+
+  if (cb) {
+    cb();
+  }
+};
+
+// When an app needs a device, it must claim before use (so that kernel
+// can handle the lock).
+devManager.prototype.enumerate = function(cb) {
+  var self = this;
+
+  function enumerated(d, acr122) {
+    var nDevice = 0;
+
+    if (d && d.length != 0) {
+      console.log(UTIL_fmt('Enumerated ' + d.length + ' devices'));
+      console.log(d);
+      nDevice = d.length;
+    } else {
+      if (d) {
+        console.log('No devices found');
+      } else {
+        /* TODO(yjlou): Review this case later (d==undefined).
+         *              Is this real lacking permission.
+         */
+        console.log('Lacking permission?');
+        do {
+          (function(cb) {
+            if (cb) window.setTimeout(function() { cb(-666); }, 0);
+          })(self.enumerators.shift());
+        } while (self.enumerators.length);
+        return;
+      }
+    }
+
+    // Found multiple devices. Create a low level SCL3711 per device.
+    for (var i = 0; i < nDevice; ++i) {
+      (function(dev, i) {
+        window.setTimeout(function() {
+            chrome.usb.claimInterface(dev, 0, function(result) {
+              console.log(UTIL_fmt('claimed'));
+              console.log(dev);
+
+              // Push the new low level device to the devs[].
+              self.devs.push(new llSCL3711(dev, acr122));
+
+              // Only callback after the last device is claimed.
+              if (i == (nDevice - 1)) {
+                var u8 = new Uint8Array(4);
+                u8[0] = nDevice >> 24;
+                u8[1] = nDevice >> 16;
+                u8[2] = nDevice >> 8;
+                u8[3] = nDevice;
+
+                // Notify all enumerators.
+                while (self.enumerators.length) {
+                  (function(cb) {
+                    window.setTimeout(function() { if (cb) cb(0, u8); }, 20);
+                  })(self.enumerators.shift());
+                }
+              }
+            });
+          }, 0);
+      })(d[i], i);
+    }
+  };
+  /* end of enumerated() */
+
+  if (this.devs.length != 0) {
+    // Already have devices. Report number right away.
+    // TODO(yjlou): The new plugged-in NFC reader may not be detected after
+    //              the first time enumerate() is called.
+    var u8 = new Uint8Array(4);
+    u8[0] = this.devs.length >> 24;
+    u8[1] = this.devs.length >> 16;
+    u8[2] = this.devs.length >> 8;
+    u8[3] = this.devs.length;
+    if (cb) cb(0, u8);
+  } else {
+    var first = this.enumerators.length == 0;
+
+    // Queue callback.
+    this.enumerators.push(cb);
+
+    if (first) {
+      // Only first requester calls actual low level.
+      window.setTimeout(function() {
+          chrome.usb.findDevices({'vendorId': 0x04e6, 'productId': 0x5591},
+            function (d) {
+              if (d && d.length != 0) {
+                enumerated(d, false);
+              } else {
+                chrome.usb.findDevices(
+                    {'vendorId': 0x072f, 'productId': 0x2200},
+                    function (d) {
+                      if (d && d.length != 0) {
+                        enumerated(d, true);
+                      }
+                    });
+              }
+          });
+      }, 0);
+    }
+  }
+};
+
+devManager.prototype.open = function(which, who, cb) {
+  var self = this;
+  // Make sure we have enumerated devices.
+  this.enumerate(function() {
+    var dev = self.devs[which];
+    if (dev) dev.registerClient(who);
+    if (cb) { cb(dev || null); }
+  });
+};
+
+devManager.prototype.close = function(singledev, who) {
+  // De-register client from all known devices,
+  // since the client might have opened them implicitly w/ enumerate().
+  // This will thus release any device without active clients.
+  var alldevs = this.devs;
+  for (var i = 0; i < alldevs.length; ++i) {
+    var dev = alldevs[i];
+    var nremaining = dev.deregisterClient(who);
+    // TODO: uncomment when Chrome stabilizes.
+    /*
+    if (nremaining == 0) {
+      // This device has no active clients remaining.
+      // Close it so libusb releases its claim and other processes
+      // can try attach to the device.
+      this.dropDevice(dev);
+    }
+    */
+  }
+};
+
+// For console interaction.
+//  rc   - a number.
+//  data - an ArrayBuffer.
+var defaultCallback = function(rc, data) {
+  var msg = 'defaultCallback('+rc;
+  if (data) msg += ', ' + UTIL_BytesToHex(new Uint8Array(data));
+  msg += ')';
+  console.log(UTIL_fmt(msg));
+};
+
+
+// Singleton tracking available devices.
+var dev_manager = new devManager();
+
---
+++ src/tt2.js
@@ -0,0 +1,292 @@
+/*
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *     http://www.apache.org/licenses/LICENSE-2.0
+  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @fileoverview 
+ */
+
+'use strict';
+
+
+function TT2(tag_id) {
+  this.tag_id = new Uint8Array(tag_id);
+  this.type_name = null;  // vendor and its card name
+
+  /*
+   * TODO: detect at beginning -- if we have a reliable way to detect.
+   *   this.detect_type_name(cb);
+  */
+
+  this.lock_contorl = [];
+}
+
+TT2.prototype.detect_type_name = function(cb) {
+  var self = this;
+  var callback = cb;
+
+  if (this.tag_id[0] == 0x04) {
+    // NxP, Try to read page 0x10. If success, it is Ultralight C.
+    this.device.read_block(0x10, function(rc, bn) {
+      if (rc) {
+        self.type_name = "Mifare Ultralight";
+      } else {
+        self.type_name = "Mifare Ultralight C";
+      }
+
+      console.debug("[DEBUG] TT2.type_name = " + self.type_name);
+      if (callback) callback();
+    });
+  }
+}
+
+
+// read NFC Type 2 tag spec 1.0 for memory structure.
+// The callback is called with cb(NDEF Uint8Array).
+TT2.prototype.read = function(device, cb) {
+  var self = this;
+  if (!cb) cb = defaultCallback;
+  var callback = cb;
+
+  function poll_block0(rc, b0_b3) {
+    if (rc) return callback(rc);
+
+    var card = new Uint8Array(b0_b3);
+    var data = new Uint8Array(b0_b3);
+    var data_size = data[14] * 8;  // CC2: unit is 8 bytes.
+    var CC0 = data[12];            // CC0: 0xE1 = NDEF
+    var CC1 = data[13];            // CC1: version of this Type 2 tag spec.
+    var CC3 = data[15];            // CC3: b7-b4: read permission.
+
+    function check_ver(cc1) {
+      var major = (cc1 & 0xf0 ) >> 4;
+      var minor = cc1 & 0x0f;
+      if (major == 0x1) return true;
+      return false;
+    }
+    function readable(cc3) {
+      return (cc3 & 0xf0) == 0x00 ? true : false;
+    }
+
+    /* TODO: support protocol other than NDEF */
+    if (CC0 != 0xE1 || !check_ver(CC1) || !readable(CC3)) {
+      console.log("UNsupported type 2 tag: CC0=" + CC0 +
+                                        ", CC1=" + CC1 +
+                                        ", CC3=" + CC3);
+      return callback(0x0777, data.buffer);
+    }
+
+    // poll data out
+    var poll_n = Math.floor((data_size + 15) / 16);
+    var block = 4;  // data starts from block 4
+
+    function poll_block(card, block, poll_n) {
+      console.log("[DEBUG] poll_n: " + poll_n);
+      if (--poll_n < 0) {
+        defaultCallback("[DEBUG] got a type 2 tag:", card.buffer);
+
+        /* TODO: call tlv.js instead */
+        /* TODO: now pass NDEF only. Support non-NDEF in the future. */
+        for (var i = 0x10; i < card.length;) {
+          switch (card[i]) {
+          case 0x00:  /* NULL */
+            console.debug("NULL TLV");
+            i++;
+            break;
+
+          case 0x01:  /* Lock Control TLV */
+            console.debug("Found Lock Control TLV");
+
+            /* TODO: refactor and share code with Memory Control TLV */
+            var PageAddr = card[i + 2] >> 4;
+            var ByteOffset = card[i + 2] & 0xf;
+            var Size = card[i + 3];
+            if (Size == 0) Size = 256;  /* 256 bits */
+            var BytesPerPage = Math.pow(2, card[i + 4] & 0xf);
+            var BytesLockedPerLockBit = card[i + 4] >> 4;
+
+            console.debug("Lock control: " +
+                "BytesLockedPerLockBit=" + BytesLockedPerLockBit +
+                ", Size=" + Size);
+
+            var ByteAddr = PageAddr * BytesPerPage + ByteOffset;
+
+            console.info("Lock control: ByteAddr=" + ByteAddr);
+            console.info("  Locked bytes:");
+            var lock_offset = 64;
+            for (var j = 0; j < (Size + 7) / 8; j++) {
+              var k = ByteAddr + j;
+
+              if (k >= card.length) {
+                console.warn("  card[" + k + "] haven't read out yet.");
+                /* TODO: read out and continue the following parse */
+                break;
+              }
+
+              var mask = card[k];
+              console.debug("  [" + k + "]: " + mask.toString(16));
+
+              if (mask & 1) console.debug("* block-locking");
+              for (var l = 1; l < 8; l++) {
+                if (j * 8 + l >= Size) continue;
+
+                for (var s = "", m = 0;
+                     m < BytesLockedPerLockBit;
+                     lock_offset++) {
+                  s += "0x" + lock_offset.toString(16) + ", ";
+                }
+                if (mask & (1 << l)) console.info("    " + s);
+              }
+            }
+
+            i += (1/*T*/ + 1/*L*/ + card[i + 1]/*len: 3*/);
+            break;
+
+          /* TODO: 0x02 -- Memory Control TLV */
+
+          case 0xFE:  /* Terminator */
+            console.debug("Terminator TLV.");
+            return;
+
+          case 0x03: /* NDEF */
+            var len = card[i + 1];
+            if ((i + 2 + len) > card.length) {
+              console.warn("TLV len " + len + " > card len " + card.length);
+            }
+            return callback(0,
+                new Uint8Array(card.subarray(i + 2, i + 2 + len)).buffer);
+
+          default:
+            console.error("Unknown Type [" + card[i] + "]");
+            return;
+          }
+        }  /* end of for */
+      }
+
+      device.read_block(block, function(rc, bn) {
+        if (rc) return callback(rc);
+        card = UTIL_concat(card, new Uint8Array(bn));
+        return poll_block(card, block + 4, poll_n);
+      });
+    }
+    poll_block(card, block, poll_n);
+  }
+
+  device.read_block(0, poll_block0);
+}
+
+
+/* Input:
+ *   ndef - Uint8Array
+ */
+TT2.prototype.compose = function(ndef) {
+
+  var blen;  // CC2
+  var need_lock_control_tlv = 0;
+
+  if ((ndef.length + 16 /* tt2_header */
+                   + 2  /* ndef_tlv */
+                   + 1  /* terminator_tlv */) > 64) {
+    /*
+     * CC bytes of MF0ICU2 (MIFARE Ultralight-C) is OTP (One Time Program).
+     * Set to maximum available size (144 bytes).
+     */
+    blen = 144 / 8;
+    need_lock_control_tlv = 1;
+
+    /* TODO: check if the ndef.length + overhead are larger than card */
+  } else {
+    /*
+     * CC bytes of MF0ICU1 (MIFARE Ultralight) is OTP (One Time Program).
+     * Set to maximum available size (48 bytes).
+     */
+    blen = 48 / 8;
+  }
+
+  var tt2_header = new Uint8Array([
+    0x00, 0x00, 0x00, 0x00,  /* UID0, UID1, UID2, Internal0 */
+    0x00, 0x00, 0x00, 0x00,  /* UID3, UID4, UID5, UID6 */
+    0x00, 0x00, 0x00, 0x00,  /* Internal1, Internal2, Lock0, Lock1 */
+    0xe1, 0x10, blen, 0x00   /* CC0, CC1, CC2(len), CC3 */
+  ]);
+
+  var lock_control_tlv = (need_lock_control_tlv) ?
+    new Uint8Array([
+      /*T*/ 0x01,
+      /*L*/ 0x03,
+      /*V*/ 0xA0, 0x10, 0x44  /* BytesLockedPerLockBit=4, Size=16
+                               * ByteAddr=160
+                               */
+    ]) :
+    new Uint8Array([]);
+
+  var ndef_tlv = new Uint8Array([
+    0x03, ndef.length        /* NDEF Message TLV */
+  ]);
+  var terminator_tlv = new Uint8Array([
+    0xfe
+  ]);
+  var ret = UTIL_concat(tt2_header, 
+            UTIL_concat(lock_control_tlv,
+            UTIL_concat(ndef_tlv,
+            UTIL_concat(new Uint8Array(ndef),
+                        terminator_tlv))));
+  return ret;
+}
+
+
+// Input:
+//   ndef: ArrayBuffer. Just ndef is needed. TT2 header is handled.
+TT2.prototype.write = function(device, ndef, cb) {
+  if (!cb) cb = defaultCallback;
+
+  var self = this;
+  var callback = cb;
+  var card = self.compose(new Uint8Array(ndef));
+  var card_blknum = Math.floor((card.length + 3) / 4);
+
+  /* TODO: check memory size according to CC value */
+  if (card_blknum > (64 / 4)) {
+    console.warn("write_tt2() card length: " + card.length +
+                 " is larger than 64 bytes. Try to write as Ultralight-C.");
+    if (card_blknum > (192 / 4)) {
+      console.error("write_tt2() card length: " + card.length +
+                    " is larger than 192 bytes (more than Ultralight-C" +
+                    " can provide).");
+      return callback(0xbbb);
+    }
+  }
+
+  function write_block(card, block_no) {
+    if (block_no >= card_blknum) { return callback(0); }
+
+		var data = card.subarray(block_no * 4, block_no * 4 + 4);
+    if (data.length < 4) data = UTIL_concat(data,
+                                            new Uint8Array(4 - data.length));
+
+    device.write_block(block_no, data, function(rc) {
+      if (rc) return callback(rc);
+      write_block(card, block_no + 1);
+    });
+  }
+
+  /* Start from CC* fields */
+  write_block(card, 3);
+}
+
+
+TT2.prototype.emulate = function(device, ndef_obj, timeout, cb) {
+  var data = this.compose(new Uint8Array(ndef_obj.compose()));
+  return device.emulate_tag(data, timeout, cb);
+}
